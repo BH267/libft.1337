aaab70bfb1a15adf685395a0ac08a9e694a568fa
Hassan Benydir <habenydi@c4r5p8.1337.ma>
2024-11-01 15:28:40 +0100

    strmapi

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,11 +6,27 @@
 /*   By: habenydi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 14:49:35 by habenydi          #+#    #+#             */
-/*   Updated: 2024/11/01 14:56:04 by habenydi         ###   ########.fr       */
+/*   Updated: 2024/11/01 15:28:08 by habenydi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	char	*
+	char		*str;
+	unsigned int	i;
+
+	if (!s)
+		return (NULL);
+	str = malloc(ft_strlen(s) + 1);
+	if (!str)
+		return (NULL);
+	while(s[i])
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
 }
