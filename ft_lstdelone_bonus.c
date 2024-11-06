aed201ca094ec16de102ed0d38deac09427966ce
Hassan Benydir <habenydi@e2r2p12.1337.ma>
2024-11-06 21:40:49 +0100

    modf-B

--- ft_lstdelone_bonus.c
+++ ft_lstdelone_bonus.c
@@ -6,7 +6,7 @@
 /*   By: habenydi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 17:58:43 by habenydi          #+#    #+#             */
-/*   Updated: 2024/11/02 18:17:36 by habenydi         ###   ########.fr       */
+/*   Updated: 2024/11/06 21:38:22 by habenydi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_lstdelone(t_list *lst, void (*del)(void*))
 {
-	del(lst->content);
-	free(lst->next);
+	if (lst || del)
+		del(lst->content);
 	free(lst);
 }
