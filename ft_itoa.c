9e662196a16e156abfc6c191cb6d3633645f6034
Hassan benydir <147273738+BH267@users.noreply.github.com>
2024-11-11 09:38:32 +0100

    Update ft_itoa.c

--- ft_itoa.c
+++ ft_itoa.c
@@ -36,28 +36,6 @@ static int	ft_nega(int n, char *num)
 	return (n);
 }
 
-static char	*ft_minhandl(int n, char *num)
-{
-	int	i;
-
-	num = malloc(12);
-	if (!num)
-		return (NULL);
-	i = 9;
-	num[11] = '\0';
-	num[0] = '-';
-	num[10] = '8';
-	n /= 10;
-	n *= -1;
-	while (n > 0)
-	{
-		num[i] = (n % 10) + '0';
-		n /= 10;
-		i--;
-	}
-	return (num);
-}
-
 char	*ft_itoa(int n)
 {
 	char	*num;
@@ -66,7 +44,7 @@ char	*ft_itoa(int n)
 
 	num = NULL;
 	if (n == -2147483648)
-		return (ft_minhandl(n, num));
+		return (ft_strdup("-2147483648"));
 	nega = (n < 0);
 	lenum = ft_lenum (n, nega);
 	num = malloc(lenum + 1);
